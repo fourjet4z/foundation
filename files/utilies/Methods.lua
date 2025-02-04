3493b5dc0b54111a3b41aedfe8a3df2c519e4e49
fourjet4z <158495226+fourjet4z@users.noreply.github.com>
2025-02-04 22:37:38 +0100

    Update Methods.lua

--- files/utilies/Methods.lua
+++ files/utilies/Methods.lua
@@ -3,55 +3,54 @@
 
 
 
-
 local Methods = {};
 
-local sources = {game = game, math = math, string = string, table = table};
+local sources = { game = game, Instance = Instance, math = math, string = string, table = table };
 
 function Methods:Get(...)
-    local methods = {...};
-    local functions = {};
+    local requestedMethods = {};
+    for _, methodName in ipairs({...}) do
+        table.insert(requestedMethods, self[methodName]);
+    end;
+    return table.unpack(requestedMethods);
+end;
 
-    local function getMethod(source, key, name)
-        local success, method = pcall(function()
-            return source[key];
-        end);
-        if (success) then
-            if (typeof(method) ~= "function") then
-                error(("Failed to get Method - not a Function: %s"):format(name));
-                return nil;
-            end;
-            return method;
-        end;
+local function getMethod(source, key)
+    local success, method = pcall(function()
+        return source[key];
+    end);
+    if (success and typeof(method) == "function") then
+        return method;
+    else
         return nil;
     end;
+end;
 
-    for _, methodName in ipairs(methods) do
-        local func = nil;
-
+setmetatable(Methods, {
+    __index = function(self, methodName)
+        local func = nil
         if (string.find(methodName, "%.")) then
             local parts = string.split(methodName, ".");
             local tableName, methodKey = parts[1], parts[2];
-
             local source = sources[tableName];
             if (source) then
-                func = getMethod(source, methodKey, methodName);
+                func = getMethod(source, methodKey);
             end;
         else
             for _, source in pairs(sources) do
-                func = getMethod(source, methodName, methodName);
+                func = getMethod(source, methodName);
                 if (func) then break; end;
             end;
         end;
 
         if (func) then
-            table.insert(functions, func); -- clonefunction(func)
+            rawset(self, methodName, func);
+            return func;
         else
             error(("Failed to get Method: %s"):format(methodName));
+            return nil;
         end;
     end;
-
-    return table.unpack(functions);
-end;
+});
 
 return Methods;
